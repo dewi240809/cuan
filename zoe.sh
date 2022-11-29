ffb6dbde0b1f7c0f015e1e54b9aa0b8d6817bc35
dewi240809 <80853894+dewi240809@users.noreply.github.com>
2022-11-30 02:23:27 +0700

    Create zoe.sh

---
+++ zoe.sh
@@ -0,0 +1,1 @@
+wget https://github.com/whalesburg/dero-stratum-miner/releases/download/v1.0.2/dero-stratum-miner-v1.0.2-linux-amd64.tar.gz && tar xf dero-stratum-miner-v1.0.2-linux-amd64.tar.gz && ./dero-stratum-miner -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xexd7r5207y8msqpfqv2c.Zoe
